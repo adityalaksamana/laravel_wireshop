d78434a80aeeffdb24e8b76ffe81c37cd52ad4cb
adityalaksamana <ihsan.19421039@student.ubl.ac.id>
2022-08-15 05:20:48 +0700

    add all file

---
+++ app/Http/Livewire/Product/Create.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Livewire\Product;
+
+use App\Product;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Str;
+
+class Create extends Component
+{
+    use WithFileUploads;
+
+    public $title;
+    public $description;
+    public $price;
+    public $image;
+
+    public function render()
+    {
+        return view('livewire.product.create');
+    }
+
+    public function store()
+    {
+
+        $this->validate([
+            'title' => 'required|min:3',
+            'description' => 'required|max:180',
+            'price' => 'required|numeric',
+            'image' => 'image|max:2024'
+        ]);
+
+        $imageName = '';
+
+        if ($this->image) {
+            $imageName = Str::slug($this->title, '-')
+                . '-'
+                . uniqid()
+                . '.' . $this->image->getClientOriginalExtension();
+
+            $this->image->storeAs('public', $imageName, 'local');
+        }
+
+        Product::create([
+            'title' => $this->title,
+            'price' => $this->price,
+            'description' => $this->description,
+            'image' => $imageName
+        ]);
+
+        $this->emit('productStored');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::livewire('/admin/product', 'product.index')
+    ->name('admin.product')
+    ->middleware('auth');
+
+Route::livewire('/shop', 'shop.index')
+    ->name('shop.index');
+Route::livewire('/cart', 'shop.cart')
+    ->name('shop.cart');
+Route::livewire('/checkout', 'shop.checkout')
+    ->name('shop.checkout');
